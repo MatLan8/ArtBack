0819299c3dde4a457b4be62fb73bfb6012ab2dde
Evitlin <evitlin@gmail.com>
2025-12-14 00:52:59 +0200

    Like Artwork

---
+++ ArtBack.Core/Handlers/Client/AddLikedArtworkHandler.cs
@@ -0,0 +1,38 @@
+﻿using ArtBack.Core.Commands.Client;
+using ArtBack.Domain.Entities;
+using ArtBack.Infrastructure;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+namespace ArtBack.Core.Handlers.Client;
+
+public class AddLikedArtworkHandler(ArtDbContext dbContext)
+    : IRequestHandler<AddLikedArtworkCommand, bool>
+{
+    public async Task<bool> Handle(AddLikedArtworkCommand request, CancellationToken cancellationToken)
+    {   
+        //ar liked
+        var existingLike = await dbContext.LikedArtworks
+            .FirstOrDefaultAsync(
+                x => x.ClientId == request.ClientId
+                     && x.ArtworkId == request.ArtworkId,
+                cancellationToken
+            );
+        
+        //taip
+        if (existingLike != null)
+            throw new InvalidOperationException("Artwork already liked");
+        
+        //ne
+        var likedArtwork = new LikedArtwork
+        {
+            ClientId = request.ClientId,
+            ArtworkId = request.ArtworkId,
+            isDeleted = false
+        };
+        
+        dbContext.LikedArtworks.AddAsync(likedArtwork, cancellationToken);
+        await dbContext.SaveChangesAsync(cancellationToken);
+        return true;
+    }
+
+}
