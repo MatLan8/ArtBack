364637e4737e9fc0db923149f1b8464e325f3f41
agne-pixel <agnemurauskaite1d@gmail.com>
2025-12-14 01:25:36 +0200

    comit

---
+++ ArtBack.Domain/Dtos/ClientDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ArtBack.Domain.DTOs;
+
+public class UpdateClientProfileDto
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Email { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? Address { get; set; }
+}
